dd764c6465dd64555432589fa078f4f04f8aac21
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 14:15:03 +0300

    Login form

---
+++ src/features/auth/LoginForm.tsx
@@ -0,0 +1,23 @@
+import { api } from "../../api/api";
+import AuthForm from "../../ui/AuthForm/AuthForm";
+import { useAuth } from "./useAuth";
+function LoginForm() {
+  const { email, setEmail, password, setPassword, handleSubmit } = useAuth(
+    api.useLoginMutation,
+    "/app"
+  );
+  return (
+    <AuthForm
+      linkText="Ещё нет аккаунта?"
+      email={email}
+      handleSubmit={handleSubmit}
+      linkTo="/register"
+      password={password}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      submitText="Login"
+    />
+  );
+}
+
+export default LoginForm;
