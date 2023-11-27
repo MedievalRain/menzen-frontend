8fb7a65709688d2256aec6b24ae926e0c9e86649
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 13:47:24 +0300

    Toast on error

---
+++ src/hooks/useError.ts
@@ -0,0 +1,64 @@
+import { SerializedError } from "@reduxjs/toolkit";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+type ErrorTypes =
+  | "JWT"
+  | "WRONG_PASSWORD"
+  | "VALIDATION_ERROR"
+  | "USER_EXISTS"
+  | "USER_NOT_EXISTS"
+  | "USER_NOT_ACTIVE"
+  | "USER_VERIFIED"
+  | "INVALID_JWT"
+  | "UNAUTHORIZED"
+  | "INTERNAL_ERROR";
+
+interface ErrorResponse {
+  error: ErrorTypes;
+}
+
+export const useError = (
+  isError: boolean,
+  error: FetchBaseQueryError | SerializedError | undefined
+) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isError && error) {
+      if ("data" in error) {
+        const errorType = (error.data as ErrorResponse).error;
+        switch (errorType) {
+          case "USER_EXISTS":
+            toast.error("User already exists");
+            break;
+          case "WRONG_PASSWORD":
+            toast.error("Wrong email or password");
+            break;
+          case "VALIDATION_ERROR":
+            toast.error("Bad input");
+            break;
+          case "INTERNAL_ERROR":
+            toast.error("Internal server error");
+            break;
+          case "USER_NOT_ACTIVE":
+            toast.error("Email is not verified");
+            break;
+          case "USER_NOT_EXISTS":
+            toast.error("User does not exist");
+            navigate("/login");
+            break;
+          case "UNAUTHORIZED":
+          case "INVALID_JWT":
+            navigate("/login");
+            break;
+          default:
+            toast.error("Uknown error");
+        }
+      } else {
+        toast.error("Connection error");
+      }
+    }
+  }, [isError]);
+};
