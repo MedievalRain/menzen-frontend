a986a496d44fffa1fd28f1069f8a846b603f2c0b
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 13:21:00 +0300

    Light theme as default

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import styles from "./App.module.scss";
+import AuthPage from "./pages/AuthPage/AuthPage";
 function App() {
+  document.documentElement.classList.remove("dark-theme");
+  document.documentElement.classList.add("light-theme");
   return (
     <div className={styles.app}>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route index element={<Navigate replace to="/app" />} />
+          <Route path="/auth" element={<AuthPage />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
