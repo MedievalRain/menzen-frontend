550009e03230b3fe41a3678ece20a04fd7f3d00e
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 13:47:39 +0300

    Styled auth form

---
+++ src/ui/AuthForm/AuthForm.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, FormEvent, SetStateAction } from "react";
+import Input from "../Input/Input";
+import styles from "./AuthForm.module.css";
+import { Link } from "react-router-dom";
+import PrimaryButton from "../PrimaryButton/PrimaryButton";
+
+interface AuthFormProps {
+  email: string;
+  setEmail: Dispatch<SetStateAction<string>>;
+  password: string;
+  setPassword: Dispatch<SetStateAction<string>>;
+  handleSubmit: (e: FormEvent) => Promise<void>;
+  submitText: string;
+  linkText: string;
+  linkTo: string;
+}
+
+function AuthForm({
+  email,
+  password,
+  setEmail,
+  setPassword,
+  handleSubmit,
+  submitText,
+  linkText,
+  linkTo,
+}: AuthFormProps) {
+  return (
+    <div className={`${styles.wrapper} shadow border-container`}>
+      <form className={styles["form-container"]} onSubmit={handleSubmit}>
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Email..."
+          name="email"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password..."
+          name="password"
+        />
+        <PrimaryButton type="submit">{submitText}</PrimaryButton>
+      </form>
+      <Link className={styles.link} to={linkTo}>
+        {linkText}
+      </Link>
+    </div>
+  );
+}
+
+export default AuthForm;
