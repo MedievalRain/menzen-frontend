589d5977034c718bccbf83002fb5465077e72480
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 21:45:35 +0300

    Adjust styling

--- src/features/tables/TableItem/TableItem.tsx
+++ src/features/tables/TableItem/TableItem.tsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
 import { Table } from "../../../api/apiTypes";
-import styles from "./TableItem.module.css";
+import styles from "./TableItem.module.scss";
 interface TableItemProps {
   table: Table;
 }
 
 function TableItem({ table }: TableItemProps) {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.item}>
+    <div className={styles.item}>
+      <button className={styles.dropdown}>...</button>
+      <Link className={styles.link} to={`/app/collection/${table.id}`}>
         <p className={styles.count}>{table.count}</p>
-        <div>{table.name}</div>
-      </div>
+        <p className={styles.name}>{table.name}</p>
+      </Link>
     </div>
   );
 }
