b60c42a8d71512dafb29b6ebcd612be29fe80b60
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 13:47:00 +0300

    Configure redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import styles from "./App.module.scss";
-import AuthPage from "./pages/AuthPage/AuthPage";
+import RegisterPage from "./pages/RegisterPage";
+import { Provider } from "react-redux";
+import store from "./store";
 function App() {
   document.documentElement.classList.remove("dark-theme");
   document.documentElement.classList.add("light-theme");
   return (
     <div className={styles.app}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Navigate replace to="/app" />} />
-          <Route path="/auth" element={<AuthPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Navigate replace to="/app" />} />
+            <Route path="/register" element={<RegisterPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ src/api/api.ts
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { AuthData } from "./apiTypes";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_API_URL,
+  }),
+  tagTypes: ["Subs"],
+  endpoints: (builder) => ({
+    register: builder.mutation<void, AuthData>({
+      query: (authData) => ({
+        url: "/user/register",
+        method: "POST",
+        body: authData,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Subs"],
+    }),
+    login: builder.mutation<void, AuthData>({
+      query: (authData) => ({
+        url: "/user/login",
+        method: "POST",
+        body: authData,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Subs"],
+    }),
+  }),
+});
