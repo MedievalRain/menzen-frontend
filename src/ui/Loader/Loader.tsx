23276342f5c6b558c24e0e3f69c34ab401a8c881
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 15:23:57 +0300

    Loader component

---
+++ src/ui/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import styles from "./Loader.module.css";
+
+function Loader() {
+  return (
+    <div className={styles["lds-ellipsis"]}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loader;
