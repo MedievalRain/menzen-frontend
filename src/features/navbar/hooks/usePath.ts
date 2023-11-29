387aae0cfcfd0163694c63ebe9a9b5030cb39906
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-29 11:12:24 +0300

    Update usePath for collection page

--- src/features/navbar/hooks/usePath.ts
+++ src/features/navbar/hooks/usePath.ts
@@ -1,16 +1,37 @@
+import { useEffect, useState } from "react";
+import { api } from "../../../api/api";
+import { useLocation } from "react-router-dom";
+
 interface Path {
   name: string;
   route: string;
 }
 
 export const usePath = () => {
-  const paths: Path[] = [];
-  const pathnames = location.pathname.split("/").filter((x) => x);
-  if (pathnames.length > 1) {
-    if (pathnames[1] === "collections") {
-      paths.push({ name: "Коллекции", route: "/app/collections" });
+  const { data: tables } = api.useGetTablesQuery();
+  const [paths, setPaths] = useState<Path[]>([]);
+  const location = useLocation();
+  useEffect(() => {
+    const pathnames = location.pathname
+      .split("/")
+      .filter((x) => x && x != "app");
+    const newPaths: Path[] = [];
+    if (pathnames[0] === "collections") {
+      newPaths.push({ name: "Коллекции", route: "/app/collections" });
+      if (tables) {
+        if (pathnames.length > 1) {
+          const table = tables.find((table) => table.id === pathnames[1]);
+          if (table) {
+            newPaths.push({
+              name: table.name,
+              route: `/app/collections/${pathnames[1]}`,
+            });
+          }
+        }
+      }
     }
-  }
+    if (newPaths.length != 0) setPaths(newPaths);
+  }, [location.pathname]);
 
   return paths;
 };
