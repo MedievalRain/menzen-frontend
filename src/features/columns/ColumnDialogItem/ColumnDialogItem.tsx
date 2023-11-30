220e9e7ca561a4cef5d0cc324697fbb1cfd3b8dc
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-30 13:11:09 +0300

    Change column status

--- src/features/columns/ColumnDialogItem/ColumnDialogItem.tsx
+++ src/features/columns/ColumnDialogItem/ColumnDialogItem.tsx
@@ -1,4 +1,6 @@
+import { api } from "../../../api/api";
 import { Column } from "../../../api/apiTypes";
+import { useError } from "../../../hooks/useError";
 import ColumnDropdown from "../ColumnDropdown/ColumnDropdown";
 import styles from "./ColumnDialogItem.module.scss";
 interface ColumnDialogItemProps {
@@ -7,9 +9,26 @@ interface ColumnDialogItemProps {
 }
 
 function ColumnDialogItem({ column, collectionId }: ColumnDialogItemProps) {
+  const [changeStatus, { isError, error }] =
+    api.useChangeColumnsStatusMutation();
+  useError(isError, error);
   return (
     <li className={styles.item} key={column.id}>
-      <span>{column.name}</span>
+      <div className={styles["left-wrapper"]}>
+        <input
+          onChange={(e) =>
+            changeStatus({
+              collectionId,
+              columnId: column.id,
+              enabled: e.target.checked,
+            })
+          }
+          defaultChecked={column.enabled}
+          type="checkbox"
+        />
+        <span>{column.name}</span>
+      </div>
+
       <ColumnDropdown column={column} collectionId={collectionId} />
     </li>
   );
