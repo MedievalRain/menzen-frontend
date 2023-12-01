d09d2e598ddd21bcbb56490edef20d1f309954ee
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-01 16:08:40 +0300

    Update sorting store

--- src/features/columns/columnSlice.ts
+++ src/features/columns/columnSlice.ts
@@ -1,12 +1,15 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
+type NotSorted = { id: undefined; type: undefined };
+type Sorted = { id: string; type: "ASC" | "DESC" };
 export type ColumnsState = {
   columns: { [key: string]: string };
-  sortColumnId: string | undefined;
+  sorting: Sorted | NotSorted;
 };
+
 const initialState: ColumnsState = {
   columns: {},
-  sortColumnId: undefined,
+  sorting: { id: undefined, type: undefined },
 };
 
 interface SearchAction {
@@ -21,10 +24,27 @@ const columnSlice = createSlice({
     search: (state, action: PayloadAction<SearchAction>) => {
       state.columns[action.payload.columnId] = action.payload.value;
     },
-    sort: (state, action: PayloadAction<string | undefined>) => {
-      state.sortColumnId = action.payload;
+    sort: (state, action: PayloadAction<string>) => {
+      if (state.sorting.id === action.payload) {
+        switch (state.sorting.type) {
+          case "ASC":
+            state.sorting.type = "DESC";
+            break;
+          case "DESC":
+            state.sorting = {
+              id: undefined,
+              type: undefined,
+            };
+            break;
+        }
+      } else {
+        state.sorting = {
+          id: action.payload,
+          type: "ASC",
+        };
+      }
     },
   },
 });
-export const { search } = columnSlice.actions;
+export const { search, sort } = columnSlice.actions;
 export default columnSlice.reducer;
