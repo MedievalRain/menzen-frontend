3c4646f69a2c422fb35fe4fdc03a772c8d6492c1
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-01 16:09:10 +0300

    Maintain sorting state

--- src/features/columns/CollectionHeadItem/CollectionHeadItem.tsx
+++ src/features/columns/CollectionHeadItem/CollectionHeadItem.tsx
@@ -1,7 +1,9 @@
 import { Column } from "../../../api/columnApi/columnApiTypes";
 import { useAppDispatch, useAppSelector } from "../../../hooks/storeHooks";
 import Input from "../../../ui/Input/Input";
-import { search } from "../columnSlice";
+import SortDownIcon from "../../../ui/icons/SortDownIcon";
+import SortUpIcon from "../../../ui/icons/SortUpIcon";
+import { search, sort } from "../columnSlice";
 import styles from "./CollectionHeadItem.module.scss";
 interface CollectionHeadItemProps {
   column: Column;
@@ -11,20 +13,37 @@ function CollectionHeadItem({ column }: CollectionHeadItemProps) {
   const dispatch = useAppDispatch();
   const searchText =
     useAppSelector((state) => state.column.columns[column.id]) || "";
+  const { sorting } = useAppSelector((state) => state.column);
 
   return (
     <th>
       <div className={styles.item}>
-        {column.name}
-        <Input
-          value={searchText}
-          onChange={(e) =>
-            dispatch(search({ columnId: column.id, value: e.target.value }))
-          }
-          className={styles.input}
-          type="text"
-          placeholder="Поиск..."
-        />
+        <div className={styles.wrapper}>
+          <button
+            onClick={() => dispatch(sort(column.id))}
+            className={`${styles["name-wrapper"]} ${
+              sorting.id === column.id ? styles.selected : ""
+            }`}
+          >
+            <p className={styles.name}>{column.name}</p>
+
+            {sorting.id === column.id && (
+              <div className={styles["icon-wrapper"]}>
+                {sorting.type === "DESC" && <SortUpIcon />}
+                {sorting.type === "ASC" && <SortDownIcon />}
+              </div>
+            )}
+          </button>
+          <Input
+            value={searchText}
+            onChange={(e) =>
+              dispatch(search({ columnId: column.id, value: e.target.value }))
+            }
+            className={styles.input}
+            type="text"
+            placeholder="Поиск..."
+          />
+        </div>
       </div>
     </th>
   );
