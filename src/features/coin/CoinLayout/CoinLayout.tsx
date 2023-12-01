9748e44dc535234a94202ef8cf307812f136c117
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-01 19:25:52 +0300

    Values layout

--- src/features/coin/CoinLayout/CoinLayout.tsx
+++ src/features/coin/CoinLayout/CoinLayout.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { useCoin } from "../../../hooks/useCoin";
 import Loader from "../../../ui/Loader/Loader";
-
+import CoinValuesList from "../CoinValuesList/CoinValuesList";
+import PrimaryButton from "../../../ui/PrimaryButton/PrimaryButton";
+import styles from "./CoinLayout.module.scss";
+import { CoinValue } from "../../../api/coinApi/coinApiTypes";
+import CoinEditValuesList from "../CoinEditValuesList/CoinEditValuesList";
 interface CoinLayoutProps {
   coinId: string;
   collectionId: string;
@@ -8,18 +13,26 @@ interface CoinLayoutProps {
 
 function CoinLayout({ coinId, collectionId }: CoinLayoutProps) {
   const { formattedCoin: coin, isFetching } = useCoin(coinId, collectionId);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [newValues, setNewValues] = useState<CoinValue[]>([]);
   if (isFetching || !coin) return <Loader />;
 
   return (
-    <div>
-      <ul>
-        {coin.values.map((value) => (
-          <li>
-            <p>{value.name}</p>
-            <p>{value.value}</p>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.wrapper}>
+      {isEditMode ? (
+        <CoinEditValuesList coin={coin} />
+      ) : (
+        <CoinValuesList coin={coin} />
+      )}
+      {isEditMode ? (
+        <PrimaryButton onClick={() => setIsEditMode(false)}>
+          Отправить
+        </PrimaryButton>
+      ) : (
+        <PrimaryButton onClick={() => setIsEditMode(true)}>
+          Редактировать
+        </PrimaryButton>
+      )}
     </div>
   );
 }
---
+++ src/features/coin/CoinValuesList/CoinValuesList.tsx
@@ -0,0 +1,20 @@
+import { FormattedCoin } from "../../../types";
+import styles from "./CoinValuesList.module.scss";
+interface CoinValuesListProps {
+  coin: FormattedCoin;
+}
+
+function CoinValuesList({ coin }: CoinValuesListProps) {
+  return (
+    <ul className={styles.list}>
+      {coin.values.map((value) => (
+        <li key={value.id} className={styles.item}>
+          <h3 className={styles.name}>{value.name}</h3>
+          <p className={styles.value}>{value.value || "-"}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CoinValuesList;
