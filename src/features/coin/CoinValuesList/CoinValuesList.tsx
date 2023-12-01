0dbd96bbc0f9cc97eeca094f19805f01597f50eb
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-01 20:41:56 +0300

    Adjust layout

--- src/features/coin/CoinValuesList/CoinValuesList.tsx
+++ src/features/coin/CoinValuesList/CoinValuesList.tsx
@@ -1,38 +1,19 @@
-import { useState } from "react";
 import { FormattedCoin } from "../../../types";
-import Input from "../../../ui/Input/Input";
 import styles from "./CoinValuesList.module.scss";
-import PrimaryButton from "../../../ui/PrimaryButton/PrimaryButton";
 interface CoinValuesListProps {
   coin: FormattedCoin;
 }
 
 function CoinValuesList({ coin }: CoinValuesListProps) {
-  const [isEditMode, setIsEditMode] = useState(false);
   return (
-    <div className={styles.wrapper}>
-      <ul className={styles.list}>
-        {coin.values.map((value) => (
-          <li key={value.id} className={styles.item}>
-            <h3 className={styles.name}>{value.name}</h3>
-            {isEditMode ? (
-              <Input type="text" className={styles.input} placeholder="..." />
-            ) : (
-              <p className={styles.value}>{value.value || "-"}</p>
-            )}
-          </li>
-        ))}
-      </ul>
-      {isEditMode ? (
-        <PrimaryButton onClick={() => setIsEditMode(false)}>
-          Отправить
-        </PrimaryButton>
-      ) : (
-        <PrimaryButton onClick={() => setIsEditMode(true)}>
-          Редактировать
-        </PrimaryButton>
-      )}
-    </div>
+    <ul className={styles.list}>
+      {coin.values.map((value) => (
+        <li key={value.id} className={styles.item}>
+          <h3 className={styles.name}>{value.name}</h3>
+          <p className={styles.value}>{value.value || "-"}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
 
