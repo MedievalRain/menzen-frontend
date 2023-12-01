90250762ca0308c74818466c4e45d9546819186c
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-01 19:34:14 +0300

    Adjust layout

--- src/features/coin/CoinLayout/CoinLayout.tsx
+++ src/features/coin/CoinLayout/CoinLayout.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
 import { useCoin } from "../../../hooks/useCoin";
 import Loader from "../../../ui/Loader/Loader";
-import CoinValuesList from "../CoinValuesList/CoinValuesList";
-import PrimaryButton from "../../../ui/PrimaryButton/PrimaryButton";
 import styles from "./CoinLayout.module.scss";
-import { CoinValue } from "../../../api/coinApi/coinApiTypes";
-import CoinEditValuesList from "../CoinEditValuesList/CoinEditValuesList";
+import CoinValuesList from "../CoinValuesList/CoinValuesList";
 interface CoinLayoutProps {
   coinId: string;
   collectionId: string;
@@ -13,26 +9,12 @@ interface CoinLayoutProps {
 
 function CoinLayout({ coinId, collectionId }: CoinLayoutProps) {
   const { formattedCoin: coin, isFetching } = useCoin(coinId, collectionId);
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [newValues, setNewValues] = useState<CoinValue[]>([]);
+
   if (isFetching || !coin) return <Loader />;
 
   return (
     <div className={styles.wrapper}>
-      {isEditMode ? (
-        <CoinEditValuesList coin={coin} />
-      ) : (
-        <CoinValuesList coin={coin} />
-      )}
-      {isEditMode ? (
-        <PrimaryButton onClick={() => setIsEditMode(false)}>
-          Отправить
-        </PrimaryButton>
-      ) : (
-        <PrimaryButton onClick={() => setIsEditMode(true)}>
-          Редактировать
-        </PrimaryButton>
-      )}
+      <CoinValuesList coin={coin} />
     </div>
   );
 }
