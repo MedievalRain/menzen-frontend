2223005dba6d56c02163c38c98d25b7ea07bea5c
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-04 00:21:02 +0300

    Page jump buttons

--- src/features/collection/PaginationControls/PaginationControls.tsx
+++ src/features/collection/PaginationControls/PaginationControls.tsx
@@ -1,6 +1,8 @@
 import { useAppDispatch, useAppSelector } from "../../../hooks/storeHooks";
 import ArrowLeftIcon from "../../../ui/icons/ArrowLeftIcon";
 import ArrowRightIcon from "../../../ui/icons/ArrowRightIcon";
+import ArrowTwoLeft from "../../../ui/icons/ArrowTwoLeft";
+import ArrowTwoRight from "../../../ui/icons/ArrowTwoRight";
 import { setPage } from "../slice/collectionSlice";
 import { getMaxPage } from "../utils";
 import styles from "./PaginationControls.module.scss";
@@ -14,6 +16,13 @@ function PaginationControls() {
   const maxPage = getMaxPage(coins.length, pageSize);
   return (
     <div className={styles.wrapper}>
+      <button
+        disabled={page <= 1}
+        onClick={() => dispatch(setPage(1))}
+        className={styles.button}
+      >
+        <ArrowTwoLeft />
+      </button>
       <button
         disabled={page <= 1}
         onClick={() => dispatch(setPage(page - 1))}
@@ -30,6 +39,13 @@ function PaginationControls() {
         className={styles.button}
       >
         <ArrowRightIcon />
+      </button>
+      <button
+        disabled={page >= maxPage}
+        onClick={() => dispatch(setPage(maxPage))}
+        className={styles.button}
+      >
+        <ArrowTwoRight />
       </button>
     </div>
   );
