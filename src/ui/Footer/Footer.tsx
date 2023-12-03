13cddce0937c976026b477297c18c282ddcd183b
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-04 01:05:57 +0300

    change email

--- src/ui/Footer/Footer.tsx
+++ src/ui/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.scss";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <a className={styles.mailto} href="mailto:medievalrain@medievalrain.net">
+      <a className={styles.mailto} href="mailto:sorkkon@gmail.com">
         MedievalRain
       </a>
       <span>&nbsp; · 2023</span>
